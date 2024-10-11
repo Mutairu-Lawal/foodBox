4d5a159842647224c0b24408a2beb02fe79ce282
lawal <lawalmuktair@gmail.com>
2024-10-11 13:51:25 +0000

    "Updated CSS styles for navbar and menu, added JavaScript functionality for toggling menu"

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// When the element with the class "menu-icon" is clicked
 $(".menu-icon").click(() => {
-  console.log("clicked");
+  // Toggle the class "menu-active" on the element with the class "navbar--hamburger-menu"
+  $(".navbar--hamburger-menu").toggleClass("menu-active");
+
+  // Check if the element with the class "navbar--hamburger-menu" now has the class "menu-active"
+  if ($(".navbar--hamburger-menu").hasClass("menu-active")) {
+    // If it does, toggle the class "slide-in-right" on the element with the class "navbar--navlists"
+    // and remove the class "slide-out-right" from the same element
+    $(".navbar--navlists").toggleClass("slide-in-right");
+    $(".navbar--navlists").removeClass("slide-out-right");
+  } else {
+    // If it doesn't, toggle the class "slide-out-right" on the element with the class "navbar--navlists"
+    // and remove the class "slide-in-right" from the same element
+    $(".navbar--navlists").toggleClass("slide-out-right");
+    $(".navbar--navlists").removeClass("slide-in-right");
+  }
 });
